8c700729baa9999bed769fd45f78ea39d9d69e58
myj9713 <myj9713@gmail.com>
2026-01-16 00:10:19 +0900

    학습:클래스이름변경,이벤트리스너,콜백비동기

--- 자바스크립트/17-클래스-이름-변경/learn.js
+++ 자바스크립트/17-클래스-이름-변경/learn.js
@@ -69,8 +69,10 @@ const replacement = messageElement.classList.replace('is-danger', 'is-success')
 console.log('replacement:', replacement)
 if (replacement) {
   console.log('교체 성공! ✅')
+  // 교체 성공한 후, 어떤 작업을 수행하고자 한다면 여기에 코드 작성
 } else {
   console.warn('교체 실패! ⚠️')
+  // 대체 가능한 작업을 수행하고자 한다면 여기에 코드 작성
 }
 
 // 설명:
@@ -83,6 +85,26 @@ if (replacement) {
 
 // [연습] target 요소가 'text-primary' 클래스를 가지고 있는지 조건문(if)으로 확인하세요.
 // 클래스가 있다면 제거하고, 없다면 추가하는 로직을 작성해 보세요.
+const targetElement = prose.querySelector('.target')
+// console.log(targetElement)
+
+// 검토할 클래스 이름을 기억하는 변수
+let checkClassName = 'size-lg'
+
+// target 요소에 text-primary 클래스 값을 가지고 있는 지 확인
+const hasTextPrimaryClass = targetElement.classList.contains(checkClassName)
+// console.log(hasTextPrimaryClass)
+
+// 만약 해당 클래스 이름이 있다면
+if (hasTextPrimaryClass) {
+  // 해당 클래스 이름 제거
+  targetElement.classList.remove(checkClassName)
+}
+// 만약 해당 클래스 이름이 없다면
+else {
+  // 해당 클래스 이름 추가
+  targetElement.classList.add(checkClassName)
+}
 
 
 // 설명:
@@ -95,6 +117,56 @@ if (replacement) {
 
 // [연습] target 요소에 'active' 클래스를 토글 처리하세요.
 
+// 토글(toggle) => OFF -> ON -> OFF
+
+// 조건문을 사용하는 경우
+// classList.contains, classList.remove, classList.add
+
+// ----다른 타입으로 작성된 코드
+let activeClassName = 'active'
+const hasActiveClass = targetElement.classList.contains(activeClassName)
+
+// 기본 메서드 이름은 remove
+let methodName = 'remove'
+
+// 대상 요소(객체)의 클래스 이름을 제어
+// methodName 변수에 설정된 이름(문자열)의 기능 실행
+// targetElement.classList[methodName](activeClassName)
+// targetElement.classList // DOMTokenList { add, remove, ... }
+// DOMTokenList.add()    // 객체.속성
+// DOMTokenList['add']() // 객체['속성이름']
+
+// 만약 요소가 활성 클래스 이름을 포함하고 있다면
+if (!hasActiveClass) {
+  // 메서드 이름을 add로 변경
+  methodName = 'add'
+}
+
+// 메서드 이름은 가지고 있을 경우 'remove'
+// 가지고 있지 않을 경우에는 'add'
+
+// 3개가 같은 결과값의 코드
+// 변수명
+targetElement.classList[methodName](activeClassName)
+// 대괄호 표기법
+targetElement.classList['remove']('active')
+// 점 표기법
+targetElement.classList.remove('active')
+
+// -------
+
+// let activeClassName = 'active'
+// const hasActiveClass = targetElement.classList.contains(activeClassName)
+
+// if (hasActiveClass) {
+//   targetElement.classList.remove(activeClassName)
+// } else {
+//   targetElement.classList.add(activeClassName)
+// }
+
+// 편의를 위한 toggle() 메서드를 사용하는 경우
+//  classList.toggle
+// targetElement.classList.toggle(activeClassName)
 
 // 설명:
 // 다크모드 스위치나 아코디언 메뉴처럼 켰다 껐다(On/Off) 하는 UI 구현 시 가장 많이 쓰입니다.
